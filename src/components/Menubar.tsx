2b4d54e4d535001c49c707b335b012205c29e1b4
BaBa0525 <hsunhsun0525@gmail.com>
2023-03-15 02:47:18 +0800

    💄 Add hover and  tap motion

--- src/components/Menubar.tsx
+++ src/components/Menubar.tsx
@@ -4,7 +4,7 @@ import homePic from "/public/images/navbar/home.png";
 
 export const Menubar: FC = () => {
   return (
-    <nav className="fixed bottom-10 flex h-16 items-center justify-between gap-10 rounded-2xl border-[1px] border-black/10 bg-nav/20 px-10 shadow-2xl backdrop-blur-sm">
+    <nav className="fixed bottom-10 flex h-16 items-center justify-between gap-10 rounded-2xl border-[1px] border-black/10 bg-nav/20 px-10 shadow-2xl backdrop-blur-sm backdrop-brightness-100">
       <MenubarItem src={homePic} alt="home" />
     </nav>
   );
--- src/components/MenubarItem.tsx
+++ src/components/MenubarItem.tsx
@@ -1,17 +1,24 @@
+import { motion } from "framer-motion";
 import Image, { type ImageProps } from "next/image";
 import { type FC } from "react";
-
-// interface MenubarItemProp
 
 export const MenubarItem: FC<ImageProps> = ({ src, alt }) => {
   return (
     <>
-      <Image
-        className="h-10 w-10 select-none rounded-lg bg-white object-cover p-1 shadow-lg"
-        src={src}
-        alt={alt}
-        draggable={false}
-      />
+      <motion.button
+        whileHover={{
+          scale: 1.2,
+          transition: { type: "spring", stiffness: 400, damping: 10 },
+        }}
+        whileTap={{ scale: 0.9 }}
+      >
+        <Image
+          className="h-10 w-10 select-none rounded-lg bg-white object-cover p-1 shadow-lg"
+          src={src}
+          alt={alt}
+          draggable={false}
+        />
+      </motion.button>
     </>
   );
 };
