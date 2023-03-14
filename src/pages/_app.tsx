5b9c13b68f98a70a9cac79f6b9ef3e39e2b75e96
BaBa0525 <hsunhsun0525@gmail.com>
2023-03-15 00:41:56 +0800

    ✨ Add font jetbrains-mono

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { JetBrains_Mono } from "next/font/google";
 
 import "@/styles/globals.css";
 
+const jetbrains_mono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-jetbrains-mono",
+});
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${jetbrains_mono.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 };
 
 export default MyApp;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,25 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          bg: "#f7f0e0",
+          text: "#323232",
+        },
+        nav: "#f6f6f6",
+      },
+      fontFamily: {
+        "jetbrains-mono": [
+          "var(--font-jetbrains-mono)",
+          ...defaultTheme.fontFamily.mono,
+        ],
+      },
+    },
   },
   plugins: [],
 };
