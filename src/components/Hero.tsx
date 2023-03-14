269044569461417f733ee4d9a30fa14ed34f546a
BaBa0525 <hsunhsun0525@gmail.com>
2023-03-15 01:45:39 +0800

    ✨ Add hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { type FC } from "react";
+import heroPic from "/public/images/hero.jpg";
+
+export const Hero: FC = () => {
+  return (
+    <section
+      id="hsunhsun"
+      className="relative flex h-screen flex-col items-center space-y-16 pt-40"
+    >
+      <Image
+        className="h-32 w-32 rounded-full object-cover shadow-xl"
+        src={heroPic}
+        alt="Only who is smart can see it"
+      />
+      <div className="font-jetbrains-mono text-2xl font-bold text-primary-text"></div>
+    </section>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
+import { Hero } from "@/components/Hero";
 import { Layout } from "@/layouts/Layout";
 import { type NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <Layout>
-      <h1 className="font-jetbrains-mono text-lg text-red-400">hello</h1>
+      <Hero />
     </Layout>
   );
 };
