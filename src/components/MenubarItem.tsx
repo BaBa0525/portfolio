b543b0e1745fb9473b2bc90ffe51d45e59ad1d88
BaBa0525 <hsunhsun0525@gmail.com>
2023-03-16 02:06:59 +0800

    ✨ Add current page cursor

--- src/components/MenubarItem.tsx
+++ src/components/MenubarItem.tsx
@@ -1,27 +1,47 @@
 import { motion } from "framer-motion";
 import Image, { type ImageProps } from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { type FC } from "react";
+import { BsDot } from "react-icons/bs";
 
-export const MenubarItem: FC<
-  ImageProps & {
-    url: string;
-    isRendering: boolean;
-    onClick?: React.MouseEventHandler<HTMLButtonElement>;
-  }
-> = ({ src, alt, url, isRendering: render, onClick }) => {
+interface MenubarItemProps extends ImageProps {
+  href: string;
+  isRendering: boolean;
+  clickHandler?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export const MenubarItem: FC<MenubarItemProps> = ({
+  src,
+  alt,
+  href,
+  isRendering,
+  clickHandler,
+}) => {
+  const router = useRouter();
+  console.log(router.pathname);
   return (
-    <>
+    <div className="relative h-10 w-10">
+      {router.pathname === href && (
+        <motion.div
+          className="absolute bottom-0 flex w-full translate-y-4 justify-center"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 1 }}
+        >
+          <BsDot className="self-center" />
+        </motion.div>
+      )}
       <motion.button
         whileHover={{
           scale: 1.8,
           transition: { type: "spring", stiffness: 400, damping: 10 },
         }}
         whileTap={{ scale: 1.2 }}
-        onClick={onClick}
-        disabled={render}
+        onClick={clickHandler}
+        disabled={isRendering}
       >
-        <Link href={url} style={render ? { pointerEvents: "none" } : {}}>
+        <Link href={href} style={isRendering ? { pointerEvents: "none" } : {}}>
           <Image
             className="h-10 w-10 select-none rounded-lg bg-white object-cover p-1 shadow-lg"
             src={src}
@@ -30,6 +50,6 @@ export const MenubarItem: FC<
           />
         </Link>
       </motion.button>
-    </>
+    </div>
   );
 };
