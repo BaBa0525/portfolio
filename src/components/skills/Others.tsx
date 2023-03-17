9a8a2b878a0f36c35b5f48fe13d5b2e94eb32174
BaBa0525 <hsunhsun0525@gmail.com>
2023-03-18 00:00:23 +0800

    ✨ Add git icon

--- src/components/skills/Others.tsx
+++ src/components/skills/Others.tsx
@@ -1,12 +1,12 @@
 import { DockerIcon } from "../icons/others/DockerIcon";
+import { GitIcon } from "../icons/others/GitIcon";
+import { SkillsLayout } from "./SkillsLayout";
 
 export const Others = () => {
   return (
-    <div className="m-2">
-      <h2 className="my-3 -translate-x-2 text-xl">Others</h2>
-      <div className="mx-4 grid grid-cols-4 gap-3">
-        <DockerIcon className="h-10 w-10" />
-      </div>
-    </div>
+    <SkillsLayout title="Others">
+      <DockerIcon className="h-10 w-10" />
+      <GitIcon className="h-10 w-10" />
+    </SkillsLayout>
   );
 };
