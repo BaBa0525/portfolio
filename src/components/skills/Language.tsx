b26bd5d4e33ce933d284af1b96f8dadf8fe5c0e8
BaBa0525 <hsunhsun0525@gmail.com>
2023-03-20 02:45:41 +0800

    🚚 Rename TSIcon

--- src/components/skills/Language.tsx
+++ src/components/skills/Language.tsx
@@ -1,6 +1,6 @@
 import { CppIcon } from "../icons/lang/CppIcon";
 import { PythonIcon } from "../icons/lang/PythonIcon";
-import { TsIcon } from "../icons/lang/TsIcon";
+import { TSIcon } from "../icons/lang/TSIcon";
 import { SkillsLayout } from "./SkillsLayout";
 
 export const Language = () => {
@@ -8,7 +8,7 @@ export const Language = () => {
     <SkillsLayout title="Language">
       <CppIcon className="h-10 w-auto" />
       <PythonIcon className="h-10 w-auto" />
-      <TsIcon className="h-10 w-10" />
+      <TSIcon className="h-10 w-10" />
     </SkillsLayout>
   );
 };
