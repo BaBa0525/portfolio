d7f4bdec951c731dc6b0a7f6005d06e8161f9aa2
BaBa0525 <hsunhsun0525@gmail.com>
2023-03-20 21:39:48 +0800

    ✨ Add title

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,4 +1,5 @@
 import { Carousel } from "@/components/projects/Carousel";
+
 import { Layout } from "@/layouts/Layout";
 import { type NextPage } from "next";
 
@@ -7,8 +8,11 @@ const Home: NextPage = () => {
     <Layout bg="bg-projects-bg">
       <section
         id="hsunhsun"
-        className="bg-primary-project-bg relative flex h-screen flex-col items-center space-y-16 pt-28"
+        className="relative flex h-screen flex-col items-center space-y-16 pt-16"
       >
+        <h1 className="text-2xl uppercase tracking-[20px] text-[#817C74]">
+          Projects
+        </h1>
         <Carousel />
       </section>
     </Layout>
