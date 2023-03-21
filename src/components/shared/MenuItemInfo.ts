fc4d456a1730d780377dabb12b27d0558db33bb5
BaBa0525 <hsunhsun0525@gmail.com>
2023-03-21 19:19:16 +0800

    💄 update about menubar icon

--- src/components/shared/MenuItemInfo.ts
+++ src/components/shared/MenuItemInfo.ts
@@ -1,8 +1,8 @@
 import mailPic from "/public/images/mail.png";
+import aboutPic from "/public/images/navbar/about.png";
 import homePic from "/public/images/navbar/home.png";
 import lightBulbPic from "/public/images/navbar/light-bulb.png";
 import projectPic from "/public/images/navbar/projects.png";
-import vscodePic from "/public/images/navbar/vscode.png";
 
 export const MenuItemInfo = [
   {
@@ -12,7 +12,7 @@ export const MenuItemInfo = [
     color: "var(--home-bg)",
   },
   {
-    src: vscodePic,
+    src: aboutPic,
     alt: "about",
     href: "/about",
     color: "var(--about-bg)",
