aa832f9d48e737e6ecdf5ee7cbe0577a77f0c70e
BaBa0525 <hsunhsun0525@gmail.com>
2023-03-29 20:53:25 +0800

    🚸 Make icon not selectable

--- src/components/skills/Language.tsx
+++ src/components/skills/Language.tsx
@@ -6,8 +6,8 @@ import { SkillsLayout } from "./SkillsLayout";
 export const Language = () => {
   return (
     <SkillsLayout title="Language">
-      <CppIcon className="h-10 w-auto md:h-14" />
-      <PythonIcon className="h-10 w-auto md:h-14" />
+      <CppIcon className="h-10 w-auto select-none md:h-14" />
+      <PythonIcon className="h-10 w-auto select-none md:h-14" />
       <TSIcon className="h-10 w-10 md:h-14 md:w-14" />
     </SkillsLayout>
   );
